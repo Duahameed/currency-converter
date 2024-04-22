dfee49e6804770f1ed7361f87ce16405240532a5
Duahameed <167262288+Duahameed@users.noreply.github.com>
2024-04-22 09:45:50 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const currency: any = {
+  USD: 1, //base currency
+  PKR: 278,
+  INR: 83.39,
+  EUR: 0.94,
+}
+
+  let user_answer = await inquirer.prompt( 
+  [
+    {
+        name: "from",
+        message: "enter from currency",
+        type: "list",
+        choices: ["USD", "PKR", "INR", "EUR"]
+    },
+    {
+
+        name: "to",
+        message: "enter to currency",
+        type: "list",
+        choices: ["USD", "PKR", "INR","EUR"]
+    },
+    {
+        name: "amount",
+        message: "enter your amount",
+        type: "number",
+    }
+]
+);
+ let fromAmount= currency[user_answer.from]
+ let toAmount = currency[user_answer.to]
+ let amount = user_answer.amount
+ let baseAmount = amount / fromAmount
+ let convertedAmount = baseAmount * toAmount
+ console.log (convertedAmount);
+ 
